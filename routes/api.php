7927d679b05a11c95332d08c235157f895ca3dd1
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-01-28 12:29:54 -0300

    fix: live reload placed in the project.

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function (Request $request) {
-    return view('welcome');
+    return 'sdds do aaaaa';
 });
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,7 @@
 composer install
 
+php artisan key:generate
+
+mv .env.example .env
+
 php artisan serve --host=0.0.0.0 --port=80
